d70dd89b2de1b7d00fc8ef8d3edcb3e6621699d1
Venugopal  K (C) <vgk@tritechsamvit.com>
2022-05-06 05:44:31 +0530

    Test Run

---
+++ spec/statistics.spec.js
@@ -0,0 +1,7 @@
+const addFun=require("../app/statistics");
+describe("First Unit Test Case",function(){
+    it("Sample Test",function(){
+        const result=addFun(10,20);
+        expect(result).toBe(20);
+    })
+})
