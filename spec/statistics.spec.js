fd6829ba89f8226f1f6f5e0b319fe945991475d8
Venugopal K <venu.shastri@gmail.com>
2022-05-06 05:48:48 +0530

    Update statistics.spec.js

--- spec/statistics.spec.js
+++ spec/statistics.spec.js
@@ -2,6 +2,6 @@ const addFun=require("../app/statistics");
 describe("First Unit Test Case",function(){
     it("Sample Test",function(){
         const result=addFun(10,20);
-        expect(result).toBe(20);
+        expect(result).toBe(30);
     })
 })
