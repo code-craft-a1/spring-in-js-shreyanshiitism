2f5f5dde35cbbfe6ae7564563ae6b13558b87460
Sudeep Prasad <sudeep.rp@gmail.com>
2025-03-09 19:03:08 +0530

    stats expectations

--- test/statistics_test.mjs
+++ test/statistics_test.mjs
@@ -6,8 +6,8 @@ describe('Statistics', () => {
         const computedStats = computeStatistics([1.5, 8.9, 3.2, 4.5]);
         const epsilon = 0.001;
         expect(computedStats.average).to.be.closeTo(4.525, epsilon);
-        expect(computedStats.min).to.be.closeTo(1.5);
-        expect(computedStats.max).to.be.closeTo(8.9);
+        expect(computedStats.min).to.be.closeTo(1.5, epsilon);
+        expect(computedStats.max).to.be.closeTo(8.9, epsilon);
     });
     it('reports NaN for empty input', () => {
         const computedStats = computeStatistics([]);
